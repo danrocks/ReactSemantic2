459354e6e4681cb1accc55b51c10a0832c62ff7a
Danieldanieljohnkerr@hotmail.com <danieljohnkerr@hotmail.com>
2017-10-09 23:22:26 +0100

    Add project files.

---
+++ ReactSemantic2/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDom = require("react-dom");
+var HelloWorld_1 = require("./HelloWorld");
+ReactDom.render(React.createElement(HelloWorld_1.Hello, { compiler: "TypeScript", framework: "React" }), document.getElementById("root"));
+//# sourceMappingURL=app.js.map
